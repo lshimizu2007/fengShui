57d323f67fb9b658cc4d7c5fde6669a33d78212b
leticia <lshimizu2007@gmail.com>
2023-02-27 12:50:40 -0300

    Iniciando Estrutura
    Criando html, css e js.

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function Procurar(){
+
+    var nome=document.getElementById("nomeid");
+    if(nome.value !=""){
+        alert('Obrigado sr(a)'+nome.value+' os dados foram encaminhados!');
+    }
+}
